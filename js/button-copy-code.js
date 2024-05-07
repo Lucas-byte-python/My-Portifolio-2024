f1079e511ee66cf5fe4aae1f1e1773e2c7030224
Lucas-Santil <lucassantil31@gmail.com>
2024-05-07 18:25:39 -0300

    firt

--- js/button-copy-code.js
+++ js/button-copy-code.js
@@ -1,7 +1,6 @@
 document.querySelectorAll('.copy-button').forEach(button => {
     button.addEventListener('click', function() {
         var codeContent = this.parentElement.querySelector('code').innerText;
-
         navigator.clipboard.writeText(codeContent).then(function() {
             alert('Código copiado para a área de transferência!');
         }, function(err) {
